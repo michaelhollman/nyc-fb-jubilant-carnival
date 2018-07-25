167045c32b8479b2b5b2b75bc07c16c99bdb6e78
Casey Bateman <Casey.Bateman@hudl.com>
2018-07-25 13:55:59 -0500

    add comments to page

--- js/app/components/comment.js
+++ js/app/components/comment.js
@@ -1,8 +1,23 @@
 define(function(require) { 
 
+  const Handlebars = require("handlebars"); 
+
+  var source   = document.getElementById("comment-template").innerHTML;
+  var template = Handlebars.compile(source);
+
   return { 
     add: function(theEvent) { 
-      console.log(theEvent);
+      var dataFromEvent = JSON.parse(theEvent.data);
+      var name = dataFromEvent.from ? dataFromEvent.from.name : ""; 
+      var html = template({ 
+        name: name, 
+        comment: dataFromEvent.message
+      });
+      var containerElement = document.createElement("div"); 
+      containerElement.innerHTML = html; 
+
+      document.getElementById("theAreaOfBusiness").appendChild(containerElement);
+      console.log(dataFromEvent);
     }
   }
 });
--- js/app/main.js
+++ js/app/main.js
@@ -10,13 +10,13 @@ define(function(require) {
   }
   
   function startReactionServerEvents() { 
-    var source = new EventSource("https://streaming-graph.facebook.com/" + liveVideoId + "/live_reactions?access_token=" + accessToken + "&comment_rate=one_per_two_seconds&fields=from{name,id},message");
+    var source = new EventSource("https://streaming-graph.facebook.com/" + liveVideoId + "/live_reactions?access_token=" + accessToken + "&comment_rate=one_per_two_seconds&fields=from{name,id,profile_pic},message");
     source.onmessage = LoggingTheEvent; 
   }
   
   function startCommentServerEvents() { 
     var source = new EventSource("https://streaming-graph.facebook.com/" + liveVideoId + "/live_comments?access_token=" + accessToken + "&comment_rate=one_per_two_seconds&fields=from{name,id},message");
-    source.onmessage = LoggingTheEvent; 
+    source.onmessage = comment.add; 
   }
 
   startReactionServerEvents();
