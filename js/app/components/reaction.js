ff657a85e1dbac34ae193ab7f728d373d041415f
Michael Hollman <michael.hollman@hudl.com>
2018-07-25 14:58:00 -0500

    silly little reactions

--- js/app/components/reaction.js
+++ js/app/components/reaction.js
@@ -1,27 +1,56 @@
-define(function(require) { 
+define(function(require) {
+  const Handlebars = require('handlebars');
 
-  const Handlebars = require("handlebars"); 
+  const source = document.getElementById('reaction-template').innerHTML;
+  const template = Handlebars.compile(source);
+  const allReactionTypes = [
+    'LIKE',
+    'LOVE',
+    'WOW',
+    'HAHA',
+    'SAD',
+    'ANGRY',
+    'THANKFUL',
+    'PRIDE',
+  ];
 
-  var source   = document.getElementById("reaction-template").innerHTML;
-  var template = Handlebars.compile(source);
+  let lastState = undefined;
 
-  return { 
-    add: function(theEvent) { 
-      var dataFromEvent = JSON.parse(theEvent.data);
-      var name = dataFromEvent.from ? dataFromEvent.from.name : ""; 
-      var html = template({ 
-        name: name, 
-        comment: dataFromEvent.message
+  return {
+    add: function(theEvent) {
+      const data = JSON.parse(theEvent.data);
+      const newState = data.reaction_stream.reduce((accum, reaction) => {
+        accum[reaction.key] = reaction.value;
+        return accum;
+      }, {});
+
+      let delta = {};
+      allReactionTypes.forEach(reactionType => {
+        const last = lastState && lastState[reactionType];
+        const nnew = newState[reactionType];
+        const diff = nnew - (last || 0);
+        if (lastState && nnew && diff > 0) {
+          delta[reactionType] = diff;
+        }
       });
-      var containerElement = document.createElement("div"); 
-      containerElement.innerHTML = html; 
+      lastState = newState;
 
-      document.getElementById("theAreaOfBusiness").appendChild(containerElement);
-      console.log(dataFromEvent);
+      Object.keys(delta).forEach(reactionType => {
+        console.log(reactionType + ' ' + delta[reactionType]);
+        const html = template({
+          reaction: reactionType.toLocaleLowerCase(),
+        });
 
-      setTimeout(function(){ 
-        containerElement.remove(); 
-      }, 3000)
-    }
-  }
+        const containerElement = document.createElement('div');
+        containerElement.innerHTML = html;
+        document
+          .getElementById('theAreaOfBusiness')
+          .appendChild(containerElement);
+
+        setTimeout(function() {
+          containerElement.remove();
+        }, 3000);
+      });
+    },
+  };
 });
--- js/app/main.js
+++ js/app/main.js
@@ -1,25 +1,21 @@
-define(function(require) { 
+define(function(require) {
   const comment = require("app/components/comment");
   const reaction = require("app/components/reaction");
   const urlParams = new URLSearchParams(window.location.search);
   const liveVideoId = urlParams.get('vid')
   const accessToken = urlParams.get('access_token');
 
-  function LoggingTheEvent(theEvent) { 
-    console.log(theEvent);
-  }
-  
-  function startReactionServerEvents() { 
+  function startReactionServerEvents() {
     var source = new EventSource("https://streaming-graph.facebook.com/" + liveVideoId + "/live_reactions?access_token=" + accessToken + "&fields=reaction_stream");
-    source.onmessage = LoggingTheEvent; 
+    source.onmessage = reaction.add;
   }
-  
-  function startCommentServerEvents() { 
+
+  function startCommentServerEvents() {
     var source = new EventSource("https://streaming-graph.facebook.com/" + liveVideoId + "/live_comments?access_token=" + accessToken + "&comment_rate=one_hundred_per_second&fields=from{name,id},message");
-    source.onmessage = comment.add; 
+    source.onmessage = comment.add;
   }
 
   startReactionServerEvents();
   startCommentServerEvents();
-  
+
 });
